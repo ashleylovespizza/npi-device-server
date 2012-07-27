bf9b82cf299e6e5fe3bec5c45e290ad89c26fb97
Ashley Holtgraver <aholtgraver@gmail.com>
2012-07-26 22:43:37 -0400

    statically serve a json file

---
+++ npi-server.js
@@ -0,0 +1,31 @@
+
+var http = require ('http'),
+	sys  = require ('sys'),
+	nodeStatic = require('node-static/lib/node-static');
+
+
+//var NPIListener = require('./lib/npilistener');
+
+var server = http.createServer(function(request, response) {
+	var file = new nodeStatic.Server('./public', {
+		cache: false
+	});
+
+	request.addListener('end', function() {
+		if (request.url == '/npitest.json' && request.method == 'GET') {
+			response.writeHead(200, {
+				'Content-Type': 'application/x-javascript'
+			});
+			var jsonString = JSON.stringify({
+				'print': 'No Pants Island: A Subsidiary of Flimshaw Industries & Do It Go LLC'
+			})
+			response.write(jsonString);
+			response.end();
+		} else {
+
+			file.serve(request,response);
+		}
+	})
+});
+
+server.listen(8000);
